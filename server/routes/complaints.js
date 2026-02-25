6dc0a318ac4013da800d68a760f2e1d31542e0c1
nishant-uxs <agarwalnishant812@gmail.com>
2026-02-25 22:51:16 +0530

    Fix complaints route - Remove undefined function imports

--- server/routes/complaints.js
+++ server/routes/complaints.js
@@ -3,17 +3,13 @@ const router = express.Router();
 const {
   createComplaint,
   getComplaints,
-  getComplaintById,
-  voteComplaint,
-  getComplaintsByLocation
+  getComplaint
 } = require('../controllers/complaintController');
 const { protect } = require('../middleware/auth');
 const upload = require('../middleware/upload');
 
 router.post('/', protect, upload.array('images', 5), createComplaint);
 router.get('/', getComplaints);
-router.get('/nearby', getComplaintsByLocation);
-router.get('/:id', getComplaintById);
-router.post('/:id/vote', protect, voteComplaint);
+router.get('/:id', getComplaint);
 
 module.exports = router;
